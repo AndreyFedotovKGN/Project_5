7a6778f57d4e2bf84224981e216ca81a052a7654
Andrey Fedotov <dj_slider_90@mail.ru>
2022-07-24 16:37:49 +0500

    Исправил замечания

--- src/Main.java
+++ src/Main.java
@@ -22,54 +22,83 @@ public class Main {
         shelf[0] = 1;
         shelf[1] = 2;
         shelf[2] = 3;
-        for (int i = 0; i < 3; i++) {
-            if (i < 2) {
+//Задание 2-1
+        for (int i = 0; i < shelf.length; i++) {
+            if (i < shelf.length-1) {
                 System.out.print(shelf[i] + ",");
-            } else if (i==2) {
-                System.out.println(shelf[i]);
+            } else {
+                System.out.println(shelf[i] + " ");
             }
         }
+//Задание 3-1
+        for (int i = shelf.length-1; i < shelf.length; i--) {
+            if (i > 0) {
+                System.out.print(shelf[i] + ",");
+            } else if (i==0) {
+                System.out.println(shelf[i] + " ");
+            }
+        }
+
 // "Задание 1-2"
         float [] showcase = {1.57f,7.654f,9.986f};
+
+//Задание 2-2
         for (int i = 0; i < showcase.length; i++) {
             if (i < (showcase.length-1)) {
                 System.out.print(showcase[i] + ",");
             } else if (i==showcase.length-1)  {
-                System.out.println(showcase[i]);
+                System.out.println(showcase[i] + " ");
+            }
+        }
+//Задание 3-2
+        for (int i = showcase.length-1; i < showcase.length; i--) {
+            if (i > 0) {
+                System.out.print(showcase[i] + ",");
+            } else if (i==0) {
+                System.out.println(showcase[i] + " ");
             }
         }
 // "Задание 1-3"
         int [] pencilBox = {5,10,15,20,25};
+
+// "Задание 2-3"
         for (int i = 0; i < pencilBox.length; i++) {
             if (i < pencilBox.length-1) {
                 System.out.print(pencilBox[i] + ",");
             } else if (i==pencilBox.length-1) {
                 System.out.println(pencilBox[i]);
             }
-
         }
-
-
+// "Задание 3-3"
+        for (int i = pencilBox.length-1; i < pencilBox.length; i--) {
+            if (i > 0) {
+                System.out.print(pencilBox[i] + ",");
+            } else if (i==0) {
+                System.out.println(pencilBox[i] + " ");
+            }
+        }
 //## 2 задача
 //Пройдите по каждому из трех массивов и распечатайте все элементы всех трех массивов, начиная с первого элемента,
 // через запятую. Запятая между последним элементом одного массива и первым элементом следующего не нужна.
 
-
-        //Задание 4
+//## 3 задача
+//Теперь ваша задача – распечатать все элементы всех трех массивов, но начинать нужно не с первого элемента массива,
+// а с последнего. Элементы должны быть распечатаны через запятую, при этом элементы одного массива располагаются
+// на одной строчке, а элементы другого массива – на другой.
+//Запятая между последним элементом одного массива и первым элементом следующего не нужна.
+//
+//Задание 4
 //пройдитесь по первому целочисленному массиву и все нечетные числа в нем сделайте четными (нужно прибавить 1).
 //Важно: код должен работать с любым целочисленным массивом, поэтому для решения задания вам нужно использовать циклы.
 //Распечатайте результат преобразования в консоль.
 //
-            for (int i = 0; i < shelf.length; i++) {
+            for (int i = 0; i <shelf.length; i++) {
             if (shelf[i] % 2 != 0) {
                 System.out.print((shelf[i] += 1) + " ");
             } else {
                 System.out.print(shelf[i] + " ");
             }
         }
-
-
-
 
     }
 }
